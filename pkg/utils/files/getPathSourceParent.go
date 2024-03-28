9671588914f2ce9a6901fe8b37eb59e14b5d8af5
Icaruk <adri-rk@hotmail.com>
2024-03-28 23:36:40 +0100

    feat: watch

--- pkg/utils/files/getPathSourceParent.go
+++ pkg/utils/files/getPathSourceParent.go
@@ -5,34 +5,6 @@ import (
 	"sincro/pkg/utils/config"
 )
 
-// if path is: "D:\Dev\sincro\playground\types\user.js"
-/* and sync is
-[
-		{
-			"source": "./playground/types",
-			"destinations": [
-				"D:/Dev/sincro/playground/types_destination1",
-				"D:/Dev/sincro/playground/types_destination2"
-			]
-		},
-		{
-			"source": "./scripts",
-			"destinations": [
-				"D:/Dev/sincro/playground/types_destination1",
-				"D:/Dev/sincro/playground/types_destination2"
-			]
-		}
-]
-*/
-// iterate over sync to get sync.Source
-// sync.Source is something like: "./playground/types"
-
-// split path into parts
-// split sync.Source into parts
-
-// count how many parts are the same
-// save that number, the sync.Source with more parts will be the parent and returned
-
 type SourceWithCount struct {
 	Source        string
 	RequiredCount int
@@ -58,9 +30,6 @@ func GetPathSyncItemParent(path string, sync []config.SyncItem) config.SyncItem 
 		// Count how many parts are the same
 		count := 0
 		for i := 0; i < len(sourceParts); i++ {
-			// if sourceParts[i] == pathParts[i] {
-			// 	count++
-			// }
 
 			// Check if pathParts[i] is present in any index of sourceParts
 			for j := 0; j < len(pathParts); j++ {
